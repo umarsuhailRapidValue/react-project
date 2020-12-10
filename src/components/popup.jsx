d00d0592b14f6f2e10313a274bd896ce4837150f
umarsuhail <umarsuhailRapidValue>
2020-12-10 14:22:49 +0530

    listing

--- src/components/popup.jsx
+++ src/components/popup.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './popup.css'
-export default function Popup() {
+import CloseIcon from '@material-ui/icons/Close';
+
+export default function Popup(props) {
+    React.useEffect(() => {
+        document.body.style.overflow = "hidden"
+    })
     return (
         <div className="popup-overlay">
             <div className="popup-container">
+                <div className="row">
+                    <h3>{props.title}</h3>
+                    <CloseIcon onClick={props.handleClose} style={{float: 'right',
+    position: 'absolute',
+    top: '32px',
+    right: '18px'}}/>
+                    <hr />
+                </div>
+                <div className="row">
+                    <div className="col-md-6 float-left" style={{float:'left'}}>
+                    <img src={props.image} alt="movie-image"style={{maxHeight:"250px"}}/>
+                    </div>
+                    <div className="col-md-6">
+    <span className="text"> <p style={{fontWeight:"500"}}>Directed By:</p> {props.directedBy}</span>
+    <span className="text"><p style={{fontWeight:"500"}}>cast:</p> {props.cast}</span>
+    <hr/>
+    <span className="rating">Movie Plot: {props.plot}</span>
+
+                    </div>
+                </div>
+
 
             </div>
         </div>
--- src/layout/GridLayout.jsx
+++ src/layout/GridLayout.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import data from '.././data.json';
-import sampleImage from '.././assets/sample.jpg'
-import promise from 'promise'
-import Loader from '../components/loader';
-import Popup from '../components/popup'
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import data from ".././data.json";
+import sampleImage from ".././assets/sample.jpg";
+import promise from "promise";
+import Loader from "../components/loader";
+import Popup from "../components/popup";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -22,17 +22,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 function testImage(URL) {
   const imagePromise = new promise(function imagePromise(resolve, reject) {
-    const imageObj = new Image()
-    imageObj.addEventListener('load', function imgOnLoad() {
+    const imageObj = new Image();
+    imageObj.addEventListener("load", function imgOnLoad() {
       resolve(this);
     });
 
-    imageObj.addEventListener('error', function imgOnError() {
+    imageObj.addEventListener("error", function imgOnError() {
       reject();
-    })
+    });
     imageObj.src = URL;
-
-  })
+  });
   return imagePromise;
 
   // var tester=new Image();
@@ -60,77 +59,107 @@ function testImage(URL) {
 // }
 
 export default function GridLayout(props) {
-  const [loading, setLoading] = useState(false)
-  const [popup, showPopup] = useState(false)
-  const [popupData, setPopup] = useState({})
+  const [loading, setLoading] = useState(false);
+  const [popup, showPopup] = useState(false);
+  const [popupData, setPopup] = useState({});
 
-  const [images, setImages] = useState([])
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
-    data.movies.map(values => {
-      setLoading(true)
-      
-      testImage(values.posterUrl).then(
+    data.movies.map((values) => {
+      setLoading(true);
 
+      testImage(values.posterUrl).then(
         function fulfilled(img) {
-          console.log('That image is found and loaded', img);
+          console.log("That image is found and loaded", img);
           const newImage = {
             id: values.id,
-            image: img.currentSrc
-          }
-          setImages((images)=>[...images,newImage])
+            image: img.currentSrc,
+          };
+          setImages((images) => [...images, newImage]);
           // setImages(images => [...images, {
           //   id: values.id,
           //   image: img.currentSrc
           // }])
-          
-          // setImages(newImage)
 
+          // setImages(newImage)
         },
         function rejected() {
-          console.log('That image was not found');
+          console.log("That image was not found");
           setLoading(false);
         }
       );
-    })
-  }, [])
-  function handleClick(value){
+    });
+  }, []);
+  function handleClick(value) {
     console.log(value);
-    showPopup(!popup)
-    setPopup(value)
+    showPopup(!popup);
+    setPopup(value);
   }
   const [spacing, setSpacing] = React.useState(2);
   const classes = useStyles();
-  console.log(images);
+  console.log(popup);
   return (
     <div>
-
-      {loading ? <Loader /> : <Grid container className={classes.root} spacing={2}>
-        <Grid item xs={12}>
-          <Grid container justify="center" style={{ marginTop: '140px' }} spacing={2} >
-            {data.movies.map((value) => (
-              <Grid key={value.id} item onClick={()=>handleClick(value)}>
-                <Paper className={classes.paper} style={{ position: 'relative' }}>
-                  {images.map(items => {
-                    <div>
-                      <img key={items.id} src={items.image} style={{ position: 'absolute', maxWidth: 100, left: 4, top: 0, maxHeight: 140 }} />
-                      <span >{items.id}</span>
-                    </div>
-
-                  })
-                  }
-
-                </Paper>
-                <Typography variant="h6" style={{ width: 100 }} component="h2">
-                  {value.title}</Typography>
-              </Grid>
-            ))}
+      {loading ? (
+        <Loader />
+      ) : (
+        <Grid container className={classes.root} spacing={2}>
+          <Grid item xs={12}>
+            <Grid
+              container
+              justify="center"
+              style={{ marginTop: "140px" }}
+              spacing={2}
+            >
+              {data.movies.map((value) => (
+                <Grid key={value.id} item onClick={() => handleClick(value)}>
+                  <Paper
+                    className={classes.paper}
+                    style={{ position: "relative" }}
+                  >
+                    {images.map((items) => {
+                      <div>
+                        <img
+                          key={items.id}
+                          src={items.image}
+                          style={{
+                            position: "absolute",
+                            maxWidth: 100,
+                            left: 4,
+                            top: 0,
+                            maxHeight: 140,
+                          }}
+                        />
+                        <span>{items.id}</span>
+                      </div>;
+                    })}
+                  </Paper>
+                  <Typography
+                    variant="h6"
+                    style={{ width: 100 }}
+                    component="h2"
+                  >
+                    {value.title}
+                  </Typography>
+                </Grid>
+              ))}
+            </Grid>
           </Grid>
         </Grid>
-
-      </Grid>}
-      {popup?<Popup/>:''}
+      )}
+      {popup ? (
+        <Popup
+          image={popupData.posterUrl}
+          directedBy={popupData.director}
+          title={popupData.title}
+          cast={popupData.actors}
+          plot={popupData.plot}
+          handleClose={() => showPopup(!popup)}
+        />
+      ) : (
+        ""
+      )}
     </div>
-
-  )
+  );
 }
