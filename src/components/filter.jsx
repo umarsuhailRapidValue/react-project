98e9a553e5c0e094e03565ba7411ee7ffb01cc15
umarsuhail <umarsuhailRapidValue>
2020-12-15 09:57:56 +0530

    css fixes

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Data from '../data.json'
 export default function Filter() {
+   const handleChange=(e)=>{
+        console.log(e.target.value);
+    }
     return (
         
-    <select id="selectGenre">
+    <select id="selectGenre" onChange={e=>handleChange(e)}>
         <option>
         Select genre</option>{Data.genres.map((x,y) => <option key={y}>{x}</option>)}</select>
     )
