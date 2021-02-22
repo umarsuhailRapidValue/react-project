618cc995a350b34b428f95945620a222d1124149
umarsuhail <umarsuhailRapidValue>
2021-02-22 17:04:54 +0530

    filter search

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -10,8 +10,8 @@ import SideBar from "./sideBar.jsx";
 import Filter from "../components/filter";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-import Downshift from "downshift";
 import { MovieContext } from "../context/movieContext";
+import Combobox from "../components/combobox.js";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -43,15 +43,7 @@ export default function Navbar(props) {
   const routePath = window.location.pathname;
   const classes = useStyles();
   const openSideBar = () => {};
-  console.log(items);
-  const itemss = [
-    { value: "apple" },
-    { value: "pear" },
-    { value: "orange" },
-    { value: "grape" },
-    { value: "banana" },
-  ];
-  console.log(itemss);
+
   return (
     <div>
       <div className={classes.root}>
@@ -80,70 +72,7 @@ export default function Navbar(props) {
             <Typography variant="h6" className={classes.title}>
               {title == "/" ? "MovieList" : "Live Score"}
             </Typography>
-            {title == "/" ?  <Downshift
-                  onChange={(selection) =>
-                    alert(`You selected ${selection.value}`)
-                  }
-                  itemToString={(item) => (item ? item.value : "")}
-                >
-                  {({
-                    getInputProps,
-                    getItemProps,
-                    getLabelProps,
-                    getMenuProps,
-                    isOpen,
-                    inputValue,
-                    highlightedIndex,
-                    selectedItem,
-                  }) => (
-                    <div style={{position:'relative',fontSize:12}}>
-                      <label {...getLabelProps()}>Enter a movie name</label>
-                      <input {...getInputProps()} />
-                      <ul
-                        {...getMenuProps()}
-                        style={{
-                          position: "absolute",
-                          display: "inline-block",
-    height: '100vh',
-    overflow: 'auto',
-    fontSize:' 12px',
-    left: '0px',
-    top: '30px',
-    listStyle: 'none',
-                        }}
-                      >
-                        {isOpen
-                          ? items
-                              .filter(
-                                (item) =>
-                                  !inputValue || item.value.includes(inputValue)
-                              )
-                              .map((item, index) => (
-                                <li
-                                  {...getItemProps({
-                                    key: item.value,
-                                    index,
-                                    item,
-                                    style: {
-                                      backgroundColor:
-                                        highlightedIndex === index
-                                          ? "lightgray"
-                                          : "black",
-                                      fontWeight:
-                                        selectedItem === item
-                                          ? "bold"
-                                          : "normal",
-                                    },
-                                  })}
-                                >
-                                  {item.value}
-                                </li>
-                              ))
-                          : null}
-                      </ul>
-                    </div>
-                  )}
-                </Downshift>: ""}
+            {title == "/" ? <Combobox items={items} />: ""}
            
             {title == "/" ? <Filter /> : ""}
 
